b71ac7c7cbf842de36b40ab550b05df3ddf835ec
Sumit Wadhwani <sumit.wadhwani@gmail.com>
2017-04-26 14:22:38 -0400

    Sumit Wadhwani: changes related to sign in with google. UI needs to be worked on.

--- dev/public/js/scripts.js
+++ dev/public/js/scripts.js
@@ -13,6 +13,33 @@ var scaper = (function(fBase){
 });
    // console.log(db);
 
+var onSignIn = function(guser){
+    alert(guser);
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+  // The signed-in user info.
+            var user = result.user;
+            console.log(token);
+            alert('welcome pendejo ' +  user.displayName);
+            console.log(user);
+  // ...
+        }).catch(function(error) {
+  // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+  // The email of the user's account used.
+            var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            console.log(errorMessage);
+  // ...
+        });
+    };
+
+
     $(document).ready(function(){
 
 
@@ -44,7 +71,6 @@ var scaper = (function(fBase){
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15 // Creates a dropdown of 15 years to control year
     });
-
 
 
 
@@ -270,7 +296,8 @@ $('#signup-tools-done').click(
 
 return {
     taskTodo:taskTodo,
-    dBase: db
+    dBase: db,
+    onSignIn: onSignIn
 }
 
 })(firebase);
